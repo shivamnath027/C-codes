79484493751ad0fdb76c340518e69b9e290253d6
Shivam Nath <shivamnath027@gmail.com>
2025-04-17 23:05:28 +0530

    Added Lists and Dict

---
+++ List_and_dictionary/List_and_dictionary/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+namespace L_and_d
+{
+    class learn
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>();
+            num.Add(1);
+            num.Add(5);
+            num.Add(4);
+            // Inbuilt functions of List
+            num.Remove(5); // Removes the first occurrence of 5
+            num.RemoveAt(0); // Removes the element at index 0
+            num.Clear(); // Removes all elements from the list
+            Console.WriteLine(num.Count);// Prints the number of elements in the list
+            Console.WriteLine(num.Contains(4)); // Checks if the list contains 4
+            Console.WriteLine(num.IndexOf(4)); // Returns the index of the first occurrence of 4
+            Console.WriteLine(num.LastIndexOf(4)); // Returns the index of the last occurrence of 4
+            Console.WriteLine(num.Capacity); // Returns the capacity of the list
+
+            // Dictionary
+            Dictionary <int,string> dict = new Dictionary<int, string>();
+            dict.Add(1, "one");
+            dict.Add(2, "two");
+            dict.Add(3, "three");
+            //Inbuilt functions of Dictionary
+            dict.Remove(2); // Removes the key-value pair with key 2
+            dict.Clear(); // Removes all key-value pairs from the dictionary
+            Console.WriteLine(dict.Count); // Prints the number of key-value pairs in the dictionary
+            Console.WriteLine(dict.ContainsKey(1)); // Checks if the dictionary contains the key 1
+            Console.WriteLine(dict.ContainsValue("one")); // Checks if the dictionary contains the value "one"
+            Console.WriteLine(dict.TryGetValue(1, out string value)); // Tries to get the value of key 1
+                                                                      // If the key is found, value will be set to the value of key 1 and the method will return true
+                                                                      // else, value will be set to null and the method will return false
+            Console.WriteLine(value); // Prints the value of key 1  
+
+            Console.WriteLine(dict.Keys); // Prints the keys of the dictionary
+            Console.WriteLine(dict.Values); // Prints the values of the dictionary
+            // Iterating through the dictionary
+            foreach (KeyValuePair<int, string> kvp in dict)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
+            //using for loop
+            for (int i = 0; i < dict.Count; i++)
+            {
+                Console.WriteLine("Key: " + dict.Keys.ElementAt(i) + ", Value: " + dict.Values.ElementAt(i));
+            }
+
+            // Iterating through the dictionary using LINQ
+            var query = from kvp in dict
+                        where kvp.Key > 1
+                        select kvp;
+            foreach (var kvp in query)
+            {
+                Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
+            }
+            
+        }
+    }
+}
---
+++ MyFirstAPI/MyFirstAPI/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        // Temporary in-memory list (just like a local DB)
+        private static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Clean Code", Author = "Robert C. Martin" },
+            new Book { Id = 2, Title = "The Pragmatic Programmer", Author = "Andrew Hunt" }
+        };
+
+        // GET: api/books 
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAll()
+        {
+            return Ok(books);
+        }
+
+        // GET: api/books/1
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetById(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+                return NotFound();
+            return Ok(book);
+        }
+
+        // POST: api/books
+        [HttpPost]
+        public ActionResult<Book> Create(Book newBook)
+        {
+            newBook.Id = books.Count + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetById), new { id = newBook.Id }, newBook);
+        }
+    }
+}
